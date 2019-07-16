fbc64c4669ca77a1b08f234b593777c0f6ed0026
bsalyp <bsalyp@cisco.com>
2019-07-16 17:42:28 +0300

    Moved key to gitignore file

--- bot.py
+++ bot.py
@@ -1,22 +1,34 @@
 import vk_api
+import random
+import key
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message})
+    vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': random.randint(1, 9999)})
 
-token = "insert_here"
+token = key.key
 
 vk = vk_api.VkApi(token=token)
 
+# Работа с сообщениями
 longpoll = VkLongPoll(vk)
 
+# Основной цикл
 for event in longpoll.listen():
+
+    # Если пришло новое сообщение
     if event.type == VkEventType.MESSAGE_NEW:
+    
+        # Если оно имеет метку для меня( то есть бота)
         if event.to_me:
+        
+            # Сообщение от пользователя
             request = event.text
-            if request == "Привет":
+            
+            # Каменная логика ответа
+            if request == "привет":
                 write_msg(event.user_id, "Хай")
-            elif request == "Пока":
-                write_msg(event.user_id, "Пока")
+            elif request == "пока":
+                write_msg(event.user_id, "Пока((")
             else:
-                write_msg(event.user_id, "Непонятно")
+                write_msg(event.user_id, "Не поняла вашего ответа...")
